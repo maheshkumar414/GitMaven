2975f0dde944f1df29c858fe63360c3bc450be10
Mahesh <maheshkumarmaramwar8@gmail.com>
2019-05-19 11:23:49 +0530

    first commit

---
+++ GitAssignment/src/main/java/com/dxc/group0/App.java
@@ -0,0 +1,13 @@
+package com.dxc.group0;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
